645f6d80e9ca9b257e87bf50dcff6afe65a7c86e
vick <vkspidey007@gmail.com>
2025-05-26 23:46:58 +0530

    Fix: 3000

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import router from "./routes/index.js";
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
